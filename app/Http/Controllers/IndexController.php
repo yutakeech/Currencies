0c3b37fb6be05a79703add3d63220c5f82e8f155
Anton Shkarin <a.shkarin@thecoders.ru>
2022-05-15 20:51:33 +0200

    Накидали стартовые вьюхи для главной страницы

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Currency;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    /**
+     * @throws \Exception
+     */
     public function index()
     {
-        return view('welcome');
+        $currencies = Currency::query()
+            ->orderBy("created_at", "DESC")->get();
+
+        return view('welcome', [
+            "currencies" => $currencies,
+        ]);
     }
+
 }
