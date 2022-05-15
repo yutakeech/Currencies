81d7c228ee53f09abb577ab79852b63ef8b3f332
Anton Shkarin <a.shkarin@thecoders.ru>
2022-05-15 22:11:43 +0200

    Добавлен функционал для аутентификации

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('title', 'Подтвердите e-mail')
+
+@section('content')
+    @include('partials.header')
+
+    <p>Необходимо подтверждение e-mail</p>
+
+    <a href="{{ route('verification.send') }}">
+        Отправить повторно
+    </a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
+
+Route::middleware("auth:web")->group(function () {
+    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware("guest:web")->group(function () {
+    Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login_process');
+
+    Route::get('/register', [\App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'register'])->name('register_process');
+
+    Route::get('/forgot', [\App\Http\Controllers\AuthController::class, 'showForgotForm'])->name('forgot');
+    Route::post('/forgot_process', [\App\Http\Controllers\AuthController::class, 'forgot'])->name('forgot_process');
+});
